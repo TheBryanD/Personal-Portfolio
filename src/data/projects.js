ca0c077a56f4d644c51d0b2c77526f6fbbce595b
Bryan <bryandavis98@hotmail.com>
2025-12-12 19:38:38 -0600

    Image import
    Import images so that they are forced to load. This should fix the 404 issue

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,23 +1,27 @@
+import PortfolioScreenshot from '../assets/Portfolio-Screenshot.png';
+import DatabaseImage from '../assets/Database-image.jpg';
+import AJDAutoScreenshot from '../assets/AJDAuto-Screenshot.png';
+
 export const projects = [
   {
     title: 'Personal Portfolio Website',
     description: 'A React based front-end portfolio website to showcase my projects and skills.',
     tags: ['React', 'Node.js', 'Tailwind', 'Vite', 'JavaScript', 'CSS', 'HTML'],
-    image: 'src/assets/Portfolio-Screenshot.png',
+    image: PortfolioScreenshot,
     link: 'https://thebryand.github.io/Personal-Portfolio/'
   },
   {
     title: 'Inventory Management System',
     description: 'A web-based inventory management system for Aristocrat to track stock levels and orders.',
     tags: ['C#', 'ASP .NET', 'JavaScript', 'Razor', 'HTML', 'PostgreSQL', 'EFCore', 'Okta'],
-    image: 'src/assets/Database-image.jpg',
+    image: DatabaseImage,
     link: 'https://thebryand.github.io/Personal-Portfolio/'
   },
   {
     title: 'AJD Laser Cutting Automation',
     description: 'A custom software solution for a small business, AJD Design, to automate creation of designs and laser cutting sheets.',
     tags: ['Python', 'Visual Basic', 'Automation', 'Web Scraping', 'JSON', 'REST APIs', 'OAuth2'],
-    image: 'src/assets/AJDAuto-Screenshot.png',
+    image: AJDAutoScreenshot,
     link: 'https://thebryand.github.io/Personal-Portfolio/'
   },
   // ...more projects
