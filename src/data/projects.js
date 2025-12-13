7106badb3872763f69d617184d53192b2071e065
Bryan <bryandavis98@hotmail.com>
2025-12-12 18:14:47 -0600

    Update path
    Hopeful fix to paths to not be relative so that browser can find them

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,21 +3,21 @@ export const projects = [
     title: 'Personal Portfolio Website',
     description: 'A React based front-end portfolio website to showcase my projects and skills.',
     tags: ['React', 'Node.js', 'Tailwind', 'Vite', 'JavaScript', 'CSS', 'HTML'],
-    image: './src/assets/Portfolio-Screenshot.png',
+    image: 'src/assets/Portfolio-Screenshot.png',
     link: 'https://thebryand.github.io/Personal-Portfolio/'
   },
   {
     title: 'Inventory Management System',
     description: 'A web-based inventory management system for Aristocrat to track stock levels and orders.',
     tags: ['C#', 'ASP .NET', 'JavaScript', 'Razor', 'HTML', 'PostgreSQL', 'EFCore', 'Okta'],
-    image: './src/assets/Database-image.jpg',
+    image: 'src/assets/Database-image.jpg',
     link: 'https://thebryand.github.io/Personal-Portfolio/'
   },
   {
     title: 'AJD Laser Cutting Automation',
     description: 'A custom software solution for a small business, AJD Design, to automate creation of designs and laser cutting sheets.',
     tags: ['Python', 'Visual Basic', 'Automation', 'Web Scraping', 'JSON', 'REST APIs', 'OAuth2'],
-    image: './src/assets/AJDAuto-Screenshot.png',
+    image: 'src/assets/AJDAuto-Screenshot.png',
     link: 'https://thebryand.github.io/Personal-Portfolio/'
   },
   // ...more projects
